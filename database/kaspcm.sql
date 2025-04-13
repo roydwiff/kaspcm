a554af5eece7912403c4578475127fb838df9fd8
roydwiff <125837538+roydwiff@users.noreply.github.com>
2025-04-13 21:21:57 +0700

    Update kaspcm.sql

--- database/kaspcm.sql
+++ database/kaspcm.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 05 Jul 2024 pada 09.34
--- Versi server: 10.4.22-MariaDB
--- Versi PHP: 7.3.33
+-- Waktu pembuatan: 13 Apr 2025 pada 16.17
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `kasrt`
+-- Database: `kaspcm`
 --
 
 -- --------------------------------------------------------
@@ -33,46 +33,43 @@ CREATE TABLE `data_kas` (
   `tanggal` date NOT NULL,
   `jumlah` varchar(50) NOT NULL,
   `jenis` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `data_kas`
 --
 
 INSERT INTO `data_kas` (`idKas`, `keterangan`, `tanggal`, `jumlah`, `jenis`) VALUES
-(30000001, 'Beli Printer L5190', '2021-07-01', '3750000', 'keluar'),
-(30000002, 'Iuran warga mingguan', '2021-07-01', '5900000', 'masuk'),
+(30000002, 'Iuran warga mingguan', '2021-07-01', '5900001', 'masuk'),
 (30000003, 'iuran warga bulanan', '2021-07-03', '7800000', 'masuk'),
 (30000004, 'pembelian dispenser', '2021-07-04', '560000', 'keluar'),
 (30000005, 'pembelian atk', '2021-07-04', '100000', 'keluar'),
-(30000006, 'Lomba agustus', '2021-07-14', '3000000', 'keluar');
+(30000006, 'Lomba agustus', '2021-07-14', '3000000', 'keluar'),
+(30000007, 'baju', '2025-04-11', '1002', 'masuk'),
+(30000008, 'ss', '2025-04-10', '2000', 'masuk'),
+(30000009, '200', '2025-04-10', '1000', 'masuk');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `data_warga`
+-- Struktur dari tabel `data_kas1`
 --
 
-CREATE TABLE `data_warga` (
-  `idWarga` int(11) NOT NULL,
-  `nik` varchar(50) NOT NULL,
-  `nama` varchar(50) NOT NULL,
-  `jekel` varchar(20) NOT NULL,
-  `tempat_lahir` varchar(20) NOT NULL,
-  `tanggal_lahir` date NOT NULL,
-  `alamat` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `data_kas1` (
+  `idKas1` int(11) NOT NULL,
+  `keterangan` varchar(100) NOT NULL,
+  `tanggal` date NOT NULL,
+  `jumlah` varchar(50) NOT NULL,
+  `jenis` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `data_warga`
+-- Dumping data untuk tabel `data_kas1`
 --
 
-INSERT INTO `data_warga` (`idWarga`, `nik`, `nama`, `jekel`, `tempat_lahir`, `tanggal_lahir`, `alamat`) VALUES
-(1, '111222333444555', 'Yohan Riki', 'laki-laki', 'Pati', '1996-10-16', 'Cilincing, Jakarta Utara'),
-(3, '222333111444555', 'Ahmad Sidiq', 'laki-laki', 'Semarang', '1995-02-10', 'bekasi utara'),
-(4, '1112221113334444', 'Echa Jesica', 'perempuan', 'Yogyakarta', '2000-02-16', 'bekasi utara'),
-(5, '111222111222444', 'Rudi Hidayat', 'laki-laki', 'Pati', '1999-06-09', 'Jakarta Utara'),
-(6, '333222111444555', 'Elisabet', 'perempuan', 'Semarang', '2001-06-07', 'Jakarta Utara');
+INSERT INTO `data_kas1` (`idKas1`, `keterangan`, `tanggal`, `jumlah`, `jenis`) VALUES
+(40000001, 'dd', '2025-04-11', '12344', 'masuk'),
+(40000002, 'asdau', '2025-04-11', '1003', 'keluar');
 
 -- --------------------------------------------------------
 
@@ -85,7 +82,7 @@ CREATE TABLE `tabel_kredit` (
   `keterangan` varchar(255) NOT NULL,
   `jumlah` decimal(10,2) NOT NULL,
   `tanggal` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -102,7 +99,7 @@ CREATE TABLE `users` (
   `is_active` tinyint(4) NOT NULL,
   `role_id` tinyint(4) NOT NULL,
   `email` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `users`
@@ -110,8 +107,8 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`user_id`, `user`, `username`, `password`, `img`, `is_active`, `role_id`, `email`) VALUES
 (3, 'Administrator', 'admin', '$2y$10$6v.9Z.2QHCs9yZxgCM5.cu3L0ltG.8/5ma5Fs5vr6KS9yQnNjg/M2', 'admin.png', 1, 1, 'admin@admin.com'),
-(4, 'Avika', 'avika', '$2y$10$WTyCYym4wxsRhSbEr093G.UFxyfEBwkH9D/A0j5Zn3JqLpthPc5XW', 'user3.png', 1, 2, 'avika@gmail.com'),
-(5, 'Audia', 'audia', '$2y$10$Eehpk2YyoS7agkOHMMtw0eF/WUmudl8gGRjpVNRXH/qH/MQxN.A72', 'user2.png', 1, 3, 'audia@gmail.com');
+(5, 'Audia', 'audia', '$2y$10$Eehpk2YyoS7agkOHMMtw0eF/WUmudl8gGRjpVNRXH/qH/MQxN.A72', 'user2.png', 1, 3, 'audia@gmail.com'),
+(13, 'JAWI', 'uwi', '$2y$10$UKYn3XZ6WRq5EWwrh1UPi.K8zqPZ80VYCTJDsAxDxMHqQS74.75EG', 'avatar.png', 1, 5, '');
 
 -- --------------------------------------------------------
 
@@ -122,7 +119,7 @@ INSERT INTO `users` (`user_id`, `user`, `username`, `password`, `img`, `is_activ
 CREATE TABLE `user_role` (
   `id` int(11) NOT NULL,
   `role` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `user_role`
@@ -130,8 +127,8 @@ CREATE TABLE `user_role` (
 
 INSERT INTO `user_role` (`id`, `role`) VALUES
 (1, 'Admin'),
-(2, 'RT'),
-(3, 'Bendahara');
+(3, 'Unit1'),
+(5, 'Unit2');
 
 --
 -- Indexes for dumped tables
@@ -144,10 +141,10 @@ ALTER TABLE `data_kas`
   ADD PRIMARY KEY (`idKas`);
 
 --
--- Indeks untuk tabel `data_warga`
+-- Indeks untuk tabel `data_kas1`
 --
-ALTER TABLE `data_warga`
-  ADD PRIMARY KEY (`idWarga`);
+ALTER TABLE `data_kas1`
+  ADD PRIMARY KEY (`idKas1`);
 
 --
 -- Indeks untuk tabel `tabel_kredit`
@@ -175,13 +172,13 @@ ALTER TABLE `user_role`
 -- AUTO_INCREMENT untuk tabel `data_kas`
 --
 ALTER TABLE `data_kas`
-  MODIFY `idKas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30000007;
+  MODIFY `idKas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30000010;
 
 --
--- AUTO_INCREMENT untuk tabel `data_warga`
+-- AUTO_INCREMENT untuk tabel `data_kas1`
 --
-ALTER TABLE `data_warga`
-  MODIFY `idWarga` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+ALTER TABLE `data_kas1`
+  MODIFY `idKas1` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40000003;
 
 --
 -- AUTO_INCREMENT untuk tabel `tabel_kredit`
@@ -193,13 +190,13 @@ ALTER TABLE `tabel_kredit`
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT untuk tabel `user_role`
 --
 ALTER TABLE `user_role`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
